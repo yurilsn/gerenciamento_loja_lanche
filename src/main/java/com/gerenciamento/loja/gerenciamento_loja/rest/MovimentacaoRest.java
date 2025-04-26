78428a7e27e407f66f9d3c5d726e591eff57cdfc
yurilsn <luizyuri57@gmail.com>
2025-04-26 19:52:13 -0300

    feat: implementando busca por todas as movimentações

--- src/main/java/com/gerenciamento/loja/gerenciamento_loja/rest/MovimentacaoRest.java
+++ src/main/java/com/gerenciamento/loja/gerenciamento_loja/rest/MovimentacaoRest.java
@@ -4,22 +4,28 @@ package com.gerenciamento.loja.gerenciamento_loja.rest;
 import com.gerenciamento.loja.gerenciamento_loja.domain.Movimentacao;
 import com.gerenciamento.loja.gerenciamento_loja.repository.MovimentacaoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/movimentacao")
 @AllArgsConstructor
 public class MovimentacaoRest {
 
-    private final MovimentacaoRepository movimentacaoRepository;
+    private MovimentacaoRepository movimentacaoRepository;
 
     @PostMapping
     public Movimentacao save(@RequestBody Movimentacao movimentacao){
         return movimentacaoRepository.save(movimentacao);
+    }
+
+
+    @GetMapping
+    public List<Movimentacao> findAll(@RequestBody Movimentacao movimentacao){
+        return movimentacaoRepository.findAll();
     }
 
 }
